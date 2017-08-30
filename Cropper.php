b9c7f1ce6b9163b0ce5cf69b308bf31af8111263
andrtechno <andrew.panix@gmail.com>
2017-08-30 17:49:41 +0300

    init

---
+++ Cropper.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ *
+ * TinyMCE renders a tinyMCE js plugin for WYSIWYG editing.
+ *
+ * @author CORNER CMS <dev@corner-cms.com>
+ * @link http://www.corner-cms.com/
+ */
+
+namespace panix\ext\cropper;
+
+use Yii;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\widgets\InputWidget;
+
+class Cropper extends InputWidget {
+
+    /**
+     * @var string the language to use. Defaults to null (en).
+     */
+    public $language;
+
+    /**
+     * @var array the options for the TinyMCE JS plugin.
+     * Please refer to the TinyMCE JS plugin Web page for possible options.
+     * @see http://www.tinymce.com/wiki.php/Configuration
+     */
+    public $clientOptions = [];
+
+    /**
+     * @var bool whether to set the on change event for the editor. This is required to be able to validate data.
+     * @see https://github.com/2amigos/yii2-tinymce-widget/issues/7
+     */
+    public $triggerSaveOnBeforeValidateForm = true;
+
+    /**
+     * @inheritdoc
+     */
+    public function run() {
+        if ($this->hasModel()) {
+            echo Html::activeTextarea($this->model, $this->attribute, $this->options);
+        } else {
+            echo Html::textarea($this->name, $this->value, $this->options);
+        }
+        $this->registerClientScript();
+    }
+
+    /**
+     * Registers tinyMCE js plugin
+     */
+    protected function registerClientScript() {
+        $lang = Yii::$app->language;
+        $js = [];
+        $view = $this->getView();
+
+        TinyMceAsset::register($view);
+
+        $id = $this->options['id'];
+
+        $this->clientOptions['selector'] = "#$id";
+
+        $this->clientOptions['plugins'] = [
+            "advlist autolink lists link charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table contextmenu paste"
+        ];
+        $this->clientOptions['toolbar'] = "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image";
+
+
+        // @codeCoverageIgnoreStart
+        if ($lang !== null && $lang !== 'en') {
+            $langFile = "langs/{$lang}.js";
+            $langAssetBundle = TinyMceLangAsset::register($view);
+            $langAssetBundle->js[] = $langFile;
+            $this->clientOptions['language_url'] = $langAssetBundle->baseUrl . "/{$langFile}";
+        }
+
+        $this->clientOptions['language'] = $lang;
+        $this->clientOptions['branding'] = false;
+        // @codeCoverageIgnoreEnd
+
+        $options = Json::encode($this->clientOptions);
+
+        $js[] = "tinymce.init($options);";
+        if ($this->triggerSaveOnBeforeValidateForm) {
+            $js[] = "$('#{$id}').parents('form').on('beforeValidate', function() { tinymce.triggerSave(); });";
+        }
+        $view->registerJs(implode("\n", $js));
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+wgt-cropper
+===========
+Widget CORNER CMS
+
+[![Latest Stable Version](https://poser.pugx.org/panix/wgt-cropper/v/stable)](https://packagist.org/packages/panix/wgt-cropper) [![Total Downloads](https://poser.pugx.org/panix/wgt-cropper/downloads)](https://packagist.org/packages/panix/wgt-cropper) [![Monthly Downloads](https://poser.pugx.org/panix/wgt-cropper/d/monthly)](https://packagist.org/packages/panix/wgt-cropper) [![Daily Downloads](https://poser.pugx.org/panix/wgt-cropper/d/daily)](https://packagist.org/packages/panix/wgt-cropper) [![Latest Unstable Version](https://poser.pugx.org/panix/wgt-cropper/v/unstable)](https://packagist.org/packages/panix/wgt-cropper) [![License](https://poser.pugx.org/panix/wgt-cropper/license)](https://packagist.org/packages/panix/wgt-cropper)
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist panix/wgt-cropper "*"
+```
+
+or add
+
+```
+"panix/wgt-cropper": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by :
+
+```php
+<?php
+ ...
+ ?>
+```
+
